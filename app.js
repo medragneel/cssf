0151739eb2771f62da0b18312204ef8969753241
mshadow <medmystgon@gmail.com>
2022-09-21 17:41:16 +0100

    v1

---
+++ app.js
@@ -0,0 +1,15 @@
+//const navToggle = document.querySelector('#nav-toggle');
+//const ul = document.querySelector('nav ul');
+//navToggle.addEventListener('click',()=>{
+//    navToggle.classList.toggle('active')
+//    if(navToggle.classList.contains('active')){
+//        ul.style.display = 'block'
+//    }else{
+//        ul.style.display = 'none'
+//    }
+//    
+//})
+
+
+
+
